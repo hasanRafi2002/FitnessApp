d6917820e54af404af10241da37475ad6b6f97cc
hasanRafi2002 <rafi2002ten@gmail.com>
2025-08-08 14:44:56 +0600

    rafi

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Md Tawhid Hasan Rafi on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
